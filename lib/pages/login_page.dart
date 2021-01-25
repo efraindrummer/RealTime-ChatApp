2979d3742fe0eba3ce332679e9de8a8fdb3df0fc
Efrain May <efrain_may_75@hotmail.com>
2021-01-25 12:43:03 -0600

    pantalla del login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,8 +4,74 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('LoginPage'),
+      backgroundColor: Color(0xffF2F2F2),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            _Logo(),
+            _Form(),
+            _Labels(),
+            Text('Terminos y condiciones de uso', style: TextStyle(fontWeight: FontWeight.w200),)
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 170,
+        margin: EdgeInsets.only(top: 50),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage('assets/tag-logo.png')),
+            SizedBox(height: 20),
+            Text('Messenger', style: TextStyle(fontSize: 30))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          TextField(),
+          TextField(),
+          RaisedButton(onPressed: (){}),
+        ],
+      ),
+    );
+  }
+}
+
+class _Labels extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text('¿No tienes cuenta?', style: TextStyle(color: Colors.black54, fontSize: 15, fontWeight: FontWeight.w300)),
+          SizedBox(height: 10),
+          Text('Crea una ahora!', style: TextStyle(color: Colors.blue[600], fontSize: 18, fontWeight: FontWeight.bold))
+        ],
       ),
     );
   }
