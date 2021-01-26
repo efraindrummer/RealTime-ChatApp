3579a01959e63e5907dbc1ca01045264a305c107
Efrain May <efrain_may_75@hotmail.com>
2021-01-26 14:26:36 -0600

    chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -4,9 +4,41 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('ChatPage'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: <Widget>[
+            CircleAvatar(
+              child: Text('Te', style: TextStyle(fontSize: 12),),
+              backgroundColor: Colors.blueAccent,
+              maxRadius: 14,
+            ),
+            SizedBox(height: 3),
+            Text('Efrain May', style: TextStyle(color: Colors.black87, fontSize: 12))
+          ],
+        ),
+        centerTitle: true,
+        elevation: 1,
       ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (_, i) => Text('$i'),
+                reverse: true,
+              )
+            ),
+            Divider(height: 1),
+            Container(
+              //caja de texto
+              height: 100,
+              color: Colors.white,
+            )
+          ],
+        ),
+      )
     );
   }
 }
