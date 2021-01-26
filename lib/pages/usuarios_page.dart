cfaf3a3438fb9baedfe6cc01fa023f195aa80c34
Efrain May <efrain_may_75@hotmail.com>
2021-01-26 13:32:56 -0600

    pantalla de usuarios

--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,12 +1,57 @@
+import 'package:chatpp/models/usuario.dart';
 import 'package:flutter/material.dart';
 
-class UsuariosPage extends StatelessWidget {
+class UsuariosPage extends StatefulWidget {
+  @override
+  _UsuariosPageState createState() => _UsuariosPageState();
+}
+
+class _UsuariosPageState extends State<UsuariosPage> {
+
+  final usuarios = [
+    Usuario(uid: '1', nombre: 'Efrain', email: 'test1@gmail.com', online: true),
+    Usuario(uid: '2', nombre: 'Fernando', email: 'test2@gmail.com', online: false),
+    Usuario(uid: '3', nombre: 'Christina', email: 'test3@gmail.com', online: false),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('UsuariosPage'),
+      appBar: AppBar(
+        title: Text('Mi nombre', style: TextStyle(color: Colors.black54),),
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.exit_to_app, color: Colors.black54),
+          onPressed: (){},
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: Icon(Icons.check_circle, color: Colors.blue[400]),
+            //Icon(Icons.offline_bolt, color: Colors.red,),
+          )
+        ],
       ),
+      body: ListView.separated(
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (_, i) => ListTile(
+          title: Text(usuarios[i].nombre),
+          leading: CircleAvatar(
+            child: Text(usuarios[i].nombre.substring(0, 2)),
+          ),
+          trailing: Container(
+            width: 10,
+            height: 10,
+            decoration: BoxDecoration(
+              color: usuarios[i].online ? Colors.green[300] : Colors.red,
+              borderRadius: BorderRadius.circular(100)
+            ),
+          ),
+        ), 
+        separatorBuilder: (_, i) => Divider(), 
+        itemCount: usuarios.length
+      )
     );
   }
 }
