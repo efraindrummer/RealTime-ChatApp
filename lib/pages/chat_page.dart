e210abd38d8c8a746551f1b4815bffa5d01cfabd
Efrain May <efrain_may_75@hotmail.com>
2021-01-26 14:52:44 -0600

    caja de texto del chat

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,6 +1,18 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+
+  final _textController = new TextEditingController();
+  final _focusNode = new FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,13 +44,58 @@ class ChatPage extends StatelessWidget {
             ),
             Divider(height: 1),
             Container(
-              //caja de texto
-              height: 100,
               color: Colors.white,
+              child: _inputChat(),
             )
           ],
         ),
       )
     );
+  }
+
+  Widget _inputChat(){
+    
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmit,
+                onChanged: (String texto){
+                  //cuando hay un valor para poder enviar  el mensaje
+                },
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Enviar mensaje'
+                ),
+                focusNode: _focusNode,
+              ),
+            ),
+            //Boton de enviar
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: Platform.isIOS ? CupertinoButton(
+                child: Text('Enviar'), 
+                onPressed: null
+              ) : Container(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconButton(
+                  icon: Icon(Icons.send, color: Colors.blue[400]),
+                  onPressed: (){},
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _handleSubmit(String texto){
+    print(texto);
+    _textController.clear();
+    _focusNode.requestFocus();
   }
 }
