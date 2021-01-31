279ed0c307750ec5b8735d7c69089d4b0ae92cc6
Efrain May <efrain_may_75@hotmail.com>
2021-01-30 20:21:55 -0600

    cambiar el icono para saber si esta cnonectado al socket - usuariosPage

--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -45,8 +45,9 @@ class _UsuariosPageState extends State<UsuariosPage> {
         actions: [
           Container(
             margin: EdgeInsets.only(right: 10),
-            child: Icon(Icons.check_circle, color: Colors.blue[400]),
-            //Icon(Icons.offline_bolt, color: Colors.red,),
+            child: (socketService.serverStatus == ServerStatus.Online)
+            ? Icon(Icons.check_circle, color: Colors.blue[400])
+            : Icon(Icons.offline_bolt, color: Colors.red,),
           )
         ],
       ),
